934d8b3a37bb00cbdc886ceb95de8642043dcf61
slmth <66906729+slmth@users.noreply.github.com>
2020-12-01 02:34:16 +0300

    Update diskan.m

--- diskan.m
+++ diskan.m
@@ -1,18 +1,18 @@
-%Diskriminant Analizi Kodları
+%Diskriminant Analizi KodlarÄ±
 
 x=[4 6;5 4;6 7;7 6;8 9;9 8;10 9;11 8;12 11];
 H=[1 1 1 1 1 0 0 0 0; 0 0 0 0 0 1 1 1 1]';
-Xg=inv(H'*H)*H'*x; %grupların ortalaması
-Xort=mean(x); %verilerin ortalaması
+Xg=inv(H'*H)*H'*x; %gruplarÄ±n ortalamasÄ±
+Xort=mean(x); %verilerin ortalamasÄ±
 
-P=x-H*Xg; %her gruptan kendi ortalamasının çıkartılmışı elde edilir
+P=x-H*Xg; %her gruptan kendi ortalamasÄ±nÄ±n Ã§Ä±kartÄ±lmÄ±ÅŸÄ± elde edilir
 W=P'*P;
 Q=H*Xg-1*Xort;
 A=Q'*Q %varyans matrisi
 R=x-1*Xort;
 T=R'*R;
 d=[-2 -1.16]'-[2.5 1.44]'%grup merkezleri arasindaki mesafe
-A2=(5*4/9)*d*d' %A değerinin farkli yolla hesaplanmasi ile A yi dogrulamis olduk 
+A2=(5*4/9)*d*d' %A deÄŸerinin farkli yolla hesaplanmasi ile A yi dogrulamis olduk 
 CW=W/9-2;
 ters_CW=1/det(CW)*[0.1333 0.6667;0.6667 -0.3333]
 k=(ters_CW)*d
@@ -38,30 +38,11 @@ ti2_fark_t2=grup2_ti2-t2
 SSA=knormalize*A*knormalize' %grup_data_sayisi(merkez+toplam merkez)ya da k'li olan formul k*A*k'
 
 SSW=sum(ti1_fark_t1.^2)+sum(ti2_fark_t2.^2) 
-%ti1'in t1'lerden farkı ve ti2'nin t2'lerden farkı değerlerinin her birinin karesinin toplanmasıyla SSW elde edilir
-%iki parcaya ayırdıgımız icin iki kısmın ayrı ayrı kareler toplamını alıp
-%en son tek bir toplam değer elde ettim 
+%ti1'in t1'lerden farkÄ± ve ti2'nin t2'lerden farkÄ± deÄŸerlerinin her birinin karesinin toplanmasÄ±yla SSW elde edilir
+%iki parcaya ayÄ±rdÄ±gÄ±mÄ±z icin iki kÄ±smÄ±n ayrÄ± ayrÄ± kareler toplamÄ±nÄ± alÄ±p
+%en son tek bir toplam deÄŸer elde ettim 
 
-%SSWyi klı formulu kullanarak dogrularsak
+%SSWyi klÄ± formulu kullanarak dogrularsak
 
 SSW_kliformul=knormalize*W*knormalize'
-
-%DHi^2 için
-
-% grup merkezleri ve xdler (R sütun1 ve sütun2den gelen xd1 ve xd2 degerleri) 
-%kullanılarak cevaplar bulunursa [-2 -1.16] ve [2.5 1.44] merkezlerimiz yerine yazıldıgında
-
-DH11=[-4+2 -1.56+1.16]*ters_CW*[-4+2 -1.56+1.16]'
-DH22=[-3+2 -3.56+1.16]*ters_CW*[-3+2 -3.56+1.16]'
-DH33=[-2+2 -0.56+1.16]*ters_CW*[-2+2 -0.56+1.16]'
-DH44=[-1+2 -1.56+1.16]*ters_CW*[-1+2 -1.56+1.16]'
-DH55=[0+2 1.44+1.16]*ters_CW*[0+2 1.44+1.16]'
-
-%ilk 5 degerden sonrasi icin ikinci parcanin d degeri cikartilarak h-xh-xi
-%bulunup ona göre islem yapıldıgında
-
-DH66=[1-2.5 0.44-1.44]*ters_CW*[1-2.5 0.44-1.44]'
-DH77=[2-2.5 1.44-1.44]*ters_CW*[2-2.5 1.44-1.44]'
-DH88=[3-2.5 0.44-1.44]*ters_CW*[3-2.5 0.44-1.44]'
-DH99=[4-2.5 3.44-1.44]*ters_CW*[4-2.5 3.44-1.44]'
 
